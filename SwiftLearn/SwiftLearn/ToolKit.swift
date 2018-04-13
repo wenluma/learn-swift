620882017ba4b19027d4727db91edd1becd1a9c1
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-04-13 10:42:05 +0800

    string """ 支持 换行 string

--- SwiftLearn/SwiftLearn/ToolKit.swift
+++ SwiftLearn/SwiftLearn/ToolKit.swift
@@ -10,6 +10,12 @@ import Foundation
 
 class ToolKit {
 	
+	/* """ 来代表 string 换行处理 */
+	private static let descrition = """
+		tool kit 是一个工具 class，用来做一些通用性的操作，比如打log，统一的逻辑处理，异常处理等。
+		都可以在这里进行
+	"""
+	
 	private static let TKVersion = "0.0" // 常量，不可变 let 声明
 	
 	public class func TKLog(funcString: String = #function, file: String = #file, line: Int = #line, items: Any...) {

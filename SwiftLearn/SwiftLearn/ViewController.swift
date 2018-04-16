afaaf79b26c43941def95d77df57fc4a669fdbda
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-04-16 09:55:14 +0800

    必要的初始化，不然报错

--- SwiftLearn/SwiftLearn/ViewController.swift
+++ SwiftLearn/SwiftLearn/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
 		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 	}
 	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.

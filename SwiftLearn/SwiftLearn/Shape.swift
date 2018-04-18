a9c0aad7c791e9b35392c461d7b2d69aae698644
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-04-18 09:52:48 +0800

    【class】init 函数
    变量未初始化的，则需要在 init 函数中初始化

--- SwiftLearn/SwiftLearn/Shape.swift
+++ SwiftLearn/SwiftLearn/Shape.swift
@@ -11,7 +11,12 @@ import Foundation
 /* class， 声明 class 开头， 内有变量，var，常量 let，已经 方法 func */
 class Shape {
 	var numberOfSides = 0 /* 变量要初始化 */
+	var name : String /* 如何没有初始值，则需要用初始化函数，进行赋值 */
 	func simpleDescription() -> String {
 		return "A shape with \(numberOfSides) sides."
+	}
+	
+	init(name: String) {
+		self.name = name
 	}
 }

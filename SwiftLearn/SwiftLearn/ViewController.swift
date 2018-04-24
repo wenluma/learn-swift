fdc98f5d5358700c0f68764c887f9535eab0eb03
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-04-24 13:40:38 +0800

    【断言操作】debug阶段使用
    assert, precondition 运行时才会知道。

--- SwiftLearn/SwiftLearn/ViewController.swift
+++ SwiftLearn/SwiftLearn/ViewController.swift
@@ -132,6 +132,16 @@ class ViewController: UIViewController {
 		if let first = Int("1"), let second = Int("3"), first < second {
 			print("\(first) < \(second)")
 		}
+		
+		/* assert 断言*/
+		assert(testNil != nil)
+		assertionFailure("is fail when assert error")
+		/* precondition 前置判断*/
+		precondition(testNil != nil)
+
+		/* fatalError 必然失败*/
+		fatalError("unimplment 未实现")
+
 	}
 	/* func 函数标示， () 内参数， -> String 返回值 类型String , to label, 可以用 _ 带他，则使用时不回会出现 to*/
 	func helloSwift(to person: String) -> String {
